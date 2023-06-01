765fd2b03353d0ee9c9e535492365c8c3b1fb960
aryoyudhanto <aryoyudhanto25399@gmail.com>
2023-06-01 19:48:35 +0700

    style: implement of responsive design in the payment success page

--- src/pages/PaymentSuccess.tsx
+++ src/pages/PaymentSuccess.tsx
@@ -31,48 +31,49 @@ const PaymentSuccess = () => {
       number2="bg-[#FF8A00] text-white "
       number3="bg-[#FF8A00] text-white "
     >
-      <div className="flex w-full my-5">
-        <div className="flex w-2/3 mx-5 justify-center items-center">
-          <div className="my-5">
-            <p className="text-5xl font-bold text-[#FF8A00]">Thank you</p>
-            <p className="mt-12 font-semibold">Order ID: {orderId}</p>
-            <p className="mt-1">
+      <div className="md:flex w-full md:my-5">
+        <div className="flex w-full md:w-2/3 md:mx-5 justify-center items-center">
+          <div className="my-7 md:my-5">
+            <p className="text-3xl md:text-5xl font-bold text-[#FF8A00]">Thank you</p>
+            <p className="mt-10 md:mt-12 text-sm md:text-base font-semibold">Order ID: {orderId}</p>
+            <p className="mt-1 text-sm md:text-base">
               Your order wil be delivered today with GO-SEND
             </p>
-            <div className="text-black flex items-center mt-5">
-              <AiOutlineArrowLeft size={15} />
+            <div className="text-black flex items-center mt-10 md:mt-12 text-sm md:text-base">
+              <AiOutlineArrowLeft />
               <Link to={"/"}>
-                <p className="hover:scale-105 transition">Go to homepage</p>
+                <p className="hover:scale-105 transition text-sm md:text-base">Go to homepage</p>
               </Link>
             </div>
           </div>
         </div>
-        <div className="w-1/3 bg-white border-l-2 border-[#FF8A00]">
-          <p className="text-3xl my-3 font-bold text-[#FF8A00] px-5">Summary</p>
-          <div className="w-full h-72 px-5">
-            <p className="text-black">10 items purchased</p>
+        <hr className="md:hidden border border-[#FF8A00]"/>
+        <div className="w-full md:w-1/3 bg-white md:border-l-2 border-[#FF8A00]">
+          <p className="text-2xl md:text-3xl my-3 font-bold text-[#FF8A00] md:px-5">Summary</p>
+          <div className="w-full h-64 md:h-72 md:px-5">
+            <p className="text-black text-xs md:text-base">10 items purchased</p>
             <hr className="my-5" />
-            <p className="text-sm">Delivery estimation</p>
-            <p className="text-green-500">Today by GO-SEND</p>
+            <p className="text-xs md:text-sm">Delivery estimation</p>
+            <p className="text-green-500 text-sm md:text-base">Today by GO-SEND</p>
             <hr className="my-5" />
-            <p className="text-sm">Payment method</p>
-            <p className="text-green-500">Bank Transfer</p>
+            <p className="text-xs md:text-sm">Payment method</p>
+            <p className="text-green-500 text-sm md:text-base">Bank Transfer</p>
           </div>
-          <div className="flex justify-between px-5">
+          <div className="flex justify-between md:px-5">
             <p className="text-sm font-normal">Cost of goods</p>
-            <p className="text-lg font-bold">500,000</p>
+            <p className="text-base font-bold">500,000</p>
           </div>
-          <div className="flex justify-between px-5">
+          <div className="flex justify-between md:px-5">
             <p className="text-sm font-normal">Dropshipping Fee</p>
-            <p className="text-lg font-bold">5,900</p>
+            <p className="text-base font-bold">5,900</p>
           </div>
-          <div className="flex justify-between px-5">
+          <div className="flex justify-between md:px-5">
             <p className="text-sm font-normal">
               <span className="font-bold">GO-SEND</span> shipment
             </p>
             <p className="text-lg font-bold">15,000</p>
           </div>
-          <div className="flex justify-between px-5 my-5">
+          <div className="flex justify-between px-0 md:px-5 my-2 md:my-5">
             <p className="text-2xl font-bold text-[#FF8A00]">Total</p>
             <p className="text-2xl font-bold text-[#FF8A00]">505,900</p>
           </div>
